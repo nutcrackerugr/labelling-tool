23caf6fbdb557f681332bebe3c0f7da46940d2f7
Manuel Francisco <manuelfrancisco@ugr.es>
2020-02-18 12:43:20 +0100

    Added support to multiple config files through wsgi.process_group and fallback profile image

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,12 @@
 from application import create_app
 
-application = create_app()
+try:
+	import mod_wsgi
+	process_group = mod_wsgi.process_group
+except:
+	process_group = ""
+
+application = create_app(config=process_group + "config")
 
 if __name__ == "__main__":
 	application.run(host="0.0.0.0")
