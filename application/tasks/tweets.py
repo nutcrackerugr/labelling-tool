ae99e966c82cabd6dcdb2bb79bc836943fa696a7
Manuel Francisco <francisco@decsai.ugr.es>
2021-01-12 18:32:26 +0100

    Fixed bug that would prevent updating tweet rank due to SQLAlchemy unawareness

--- application/tasks/tweets.py
+++ application/tasks/tweets.py
@@ -109,17 +109,10 @@ def promote_tracked_tweets_and_negative_users():
             if node["tweets"]:
                 if node["source"]["kind"] == "tracked_retweets_positive":
                         tid = random.sample(node["tweets"], 1)
-
-                        tweet = db.session.query(Tweet).filter(Tweet.id == tid).first()
-
-                        if tweet:
-                            tweet.rank = 9999 - tweet.rank
+                        tweet = db.session.query(Tweet).filter(Tweet.id == tid).update({Tweet.rank: 9999 - Tweet.rank})
                 elif len(node["positives"]) == 0:
                     for tid in random.sample(node["tweets"], min(5, len(node["tweets"]))):
-                        tweet = db.session.query(Tweet).filter(Tweet.id == tid).first()
-
-                        if tweet:
-                            tweet.rank = 999 - tweet.rank
+                        tweet = db.session.query(Tweet).filter(Tweet.id == tid).update({Tweet.rank: 999 - Tweet.rank})
     
         try:
             db.session.commit()
